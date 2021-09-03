1571f8124aec6088cef70ef9c10a3c06e3921cd4
Matias Olivera <matias.olivera@avatarla.com>
2021-09-02 21:34:10 -0300

    feat(devops): ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,6 +4,10 @@ module.exports = {
       name: 'poke-api',
       script: 'build/init.js',
       watch: false,
+      env: {
+        NODE_ENV: 'production',
+        PORT: 3010
+      },
     },
   ],
 };
