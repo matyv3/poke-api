8fa950d3677e1a3b29ea604fa086d7816137ad9e
Matias Olivera <matias.olivera@avatarla.com>
2021-09-03 18:37:47 -0300

    feat: Readme

---
+++ README.md
@@ -0,0 +1,15 @@
+
+# Pokemon REST API
+
+## Startup
+
+Rquirements:
+
+- Node.js 14.x
+- MYSQL server
+
+Run `mv .env.starter .env`
+
+Set environment variables in .env 
+
+Run `npm run build && npm start`
---
+++ pokeapi.sql
@@ -0,0 +1,16 @@
+
+INSERT INTO `expansions` (`id`, `name`) VALUES
+(1, 'Fossil'),
+(2, 'Base Set'),
+(3, 'Jungle');
+
+
+INSERT INTO `types` (`id`, `name`) VALUES
+(1, 'Agua'),
+(2, 'Fuego'),
+(3, 'Hierba'),
+(4, 'Electrico');
+
+
+INSERT INTO `users` (`id`, `email`, `name`, `password`) VALUES
+(1, 'demo@mail.com', 'demo', '$2a$10$k4lhkhC3SKBS5S7RW6XQFu05mNM/nqpxxS0UdxGA18.fDk.PH1lC2');
